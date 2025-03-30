23f8273304dc7c510ab55fb7459aa77665f4adeb
Pol <poldama99@gmail.com>
2025-03-30 11:19:43 +0200

    Add data formatting steps to script

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -1,5 +1,204 @@
+import os
+import json
+import zipfile
+import requests
 import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
+from sklearn.preprocessing import LabelEncoder
+
+#region **** Dictionaries ****
+
+# Províncies dictionary
+provincies_dict = {
+    8: 'Barcelona',
+    17: 'Girona',
+    25: 'Lleida',
+    43: 'Tarragona',
+}
+
+# Municipis dictionary
+comarques_file = pd.read_csv("data/comarques.csv")
+
+municipi_dict = {}
+current_comarca = {}
+
+for _, row in comarques_file.iterrows():
+    if row["Nivell"] == "Comarca":
+        # Store comarca info
+        current_comarca = {"codi": row["Codi"], "nom": row["Nom"]}
+    elif row["Nivell"] == "Municipi":
+        municipi_dict[row["Codi"]] = {
+            "municipi": row["Nom"],
+            "codi_comarca": current_comarca["codi"],
+            "comarca": current_comarca["nom"],
+        }
+#endregion
+
+#region **** Data cleaning and formatting functions ****
+
+#region ** CONSTANTS **
+RENAMINGS = {
+      'qualificaci_de_consum_d': 'qual_energia',
+      'qualificaci_emissions': 'qual_emissions',
+      'motiu_de_la_certificacio': 'motiu',
+      'eina_de_certificacio':'eina'
+      };
+COLUMNS_IN_USE = ['codi_poblacio', 'codi_comarca', 'codi_provincia', 'MUNDISSEC', 'metres_cadastre', 'emissions_de_co2', 'qual_energia', 'qual_emissions', 'qual_energia', 'qual_emissions', 'data_entrada', 'motiu', 'us_edifici']
+SAME_MEANING_VALUES = [
+      ['us_edifici', "Terciari", ['Terciario']],
+      ['us_edifici', "Bloc d'habitatges", ['Bloque de viviendas']],
+      ['us_edifici', "Bloc d'habitatges plurifamiliar", ['Bloque de viviendas plurifamiliar']],
+      ['us_edifici', 'Habitatge unifamiliar', ['Vivienda unifamiliar', 'Habitatge Unifamiliar']],
+      ['us_edifici', "Habitatge individual en bloc d'habitatges", ['Vivienda individual en bloque de viviendas']],
+      ['motiu', 'Lloguer', ['Alquiler']],
+      ['motiu', "Sol·licitud d'ajuts", ['Solicitud de ayudas']],
+      ['motiu', 'Compravenda', ['Compra o Venda', 'Compra o Venta']],
+      ['motiu', 'Certificació voluntària', ['Certificación voluntaria', 'Certificació voluntaria']],
+      ['motiu', "Informe d'avaluació de l'Edifici (IEE)", ['Informe de evaluación del Edificio (IEE)']],
+      ['motiu', "Altres (cap de les anteriores opcions)", ['Otros (ninguna de las anteriores opciones)']],
+      ['motiu', "Nova construcció o gran rehabilitació", ['Nova construcció', 'Nueva construcción', 'Nueva construcción  o gran rehabilitación']],
+      ['motiu', "Nova construcció - ampliació amb entitat jurídica independent", ['Nueva construcción - ampliación con entidad jurídica independiente']],
+      ['motiu', "Renovació (en cas de caducitat de l'antic certificat energètic)", ['Renovación (en caso de caducidad del antiguo certificado energético)']],
+      ['motiu', "Edifici existent de l’administració pública", ['Edifici existent de l¿administració pública', 'Edificio existente de la administración pública']],
+      ['motiu', "Edificis o parts d’edificis on es realitzin reformes o ampliacions", ['Edificis o parts d¿edificis on es realitzin reformes o ampliacions', 'Edificios o partes de edificis en los que se realicen reformas o ampliaciones']],
+      ['motiu', "Complement a informe d’avaluació de l’edifici (IEE) o a inspecció tècnica de l’edifici (ITE)", ['Complement a informe d¿avaluació de l¿edifici (IEE) o a inspecció tècnica de l¿edifici (ITE)', 'Complemento al informe de evaluación del edificio (IEE) o a inspección técnica del edificio (ITE)', 'Complement a informe davaluació de l’edifici (IEE) o a inspecció tècnica de l’edifici (ITE)']],
+      ['motiu', "Edificis o parts d’edificis (+500 m2) amb ús administratiu, sanitari, comercial, docent, restauració", ['Edificis o parts d¿edificis (+500 m2) amb ús administratiu, sanitari, comercial, docent, restauració', 'Edificios o partes de edificios (+500 m2) de uso administrativo, sanitario, comercial, docente, restauración...', "Edificis o parts d'edificis (+500 m2) amb ús administratiu, sanitari, comercial, docent, restauració"]],
+  ]
+CATEGORICAL_COLUMNS_TO_ENCODE = ['eina', 'motiu', 'us_edifici']
+#endregion
+
+def generateMundissec(df):
+  print("Generating MUNDISSEC...")
+  shapefile_url = "https://datacloud.icgc.cat/datacloud/bseccen_etrs89/shp/bseccenv10sh1f1_20240101_0.zip" # 2024 shapefile
+
+  zip_path = "data/seccions-censals.zip"
+  extract_path = "data/seccions-censals"
+
+  response = requests.get(shapefile_url)
+  with open(zip_path, "wb") as f:
+      f.write(response.content)
+
+  with zipfile.ZipFile(zip_path, "r") as zip_ref:
+      zip_ref.extractall(extract_path)
+
+  shapefile_path = [os.path.join(extract_path, f) for f in os.listdir(extract_path) if f.endswith(".shp")][0]
+  ceccsen = gpd.read_file(shapefile_path)
+
+  geometry = [Point(xy) for xy in zip(df["longitud"], df["latitud"])]
+  mundissec_gdf = gpd.GeoDataFrame(df, geometry=geometry, crs="EPSG:4326") # Convert to GeoDataFrame
+
+  # Reproject df to match the shapefile (EPSG:25831)
+  mundissec_gdf = mundissec_gdf.to_crs(epsg=25831)
+
+  # Perform spatial join
+  full_df_mundissec = gpd.sjoin(mundissec_gdf, ceccsen, how="left", predicate="within")
+
+  # Drop index column from the join
+  full_df_mundissec = full_df_mundissec.drop(columns=["index_right"])
+
+  return full_df_mundissec
+
+def renameColumns(df, renamings):
+  print("Renaming columns...")
+  dataset_columns = df.columns
+  rename_dict = {col: renamings[col] for col in renamings if col in dataset_columns}
+  return df.rename(columns=rename_dict)
+
+def reduceColumns(df, columns):
+  print("Reducing columns...")
+  reduced_dataset = df[columns]
+  return reduced_dataset
+
+def castColumns(df):
+    print("Casting the columns to their correct type...")
+    df = df.copy()
+    df.loc[:, 'codi_provincia'] = pd.to_numeric(df['codi_provincia'], errors='coerce').astype('Int64')
+    df.loc[:, 'codi_poblacio'] = pd.to_numeric(df['codi_poblacio'], errors='coerce').astype('Int64')
+    df.loc[:, 'codi_comarca'] = pd.to_numeric(df['codi_comarca'], errors='coerce').astype('Int64')
+    df.loc[:, 'MUNDISSEC'] = pd.to_numeric(df['MUNDISSEC'], errors='coerce').astype('Int64')
+    return df
+
+def groupSameMeaningValues(df, same_meaning_values):
+  print("Grouping the same meaning labels...")
+  for field_values in same_meaning_values:
+    for value in field_values[2]:
+      df.loc[df[field_values[0]] == value, field_values[0]] = field_values[1]
+
+  return df
+
+def removeOutliers(df):
+  print("Removing outliers...")
+  Q1 = df['emissions_de_co2'].quantile(0.10)
+  Q3 = df['emissions_de_co2'].quantile(0.90)
+  IQR = Q3 - Q1
+
+  upper_bound = Q3 + 1.5 * IQR
+
+  df = df.loc[df['emissions_de_co2'] < upper_bound]
+
+  return df
+
+def regenerateCodes(df, municipi_dict, provincies_dict):
+  print("Regenerating codes...")
+  df['codi_poblacio'] = df['MUNDISSEC'] // 100000
+
+  def get_values(codi):
+      dict_entry = municipi_dict.get(codi)
+      if dict_entry:
+          return dict_entry['municipi'], dict_entry['codi_comarca'], dict_entry['comarca']
+      else:
+          return None, None, None
+
+  df[['poblacio', 'codi_comarca', 'comarca']] = df['codi_poblacio'].map(get_values).apply(pd.Series)
+
+  df['codi_provincia'] = df['codi_poblacio'] // 10000
+  df['nom_provincia'] = df['codi_provincia'].map(provincies_dict)
+
+  return df
+
+def encode_categorical_columns(df, categorical_columns):
+  print("Encoding categorical columns...")
+  encoders = {}
+  label_mapping = {}
+
+  df_columns = df.columns
+
+  for column in categorical_columns:
+     if column in df_columns:
+      encoders[column] = LabelEncoder()
+      df[column] = encoders[column].fit_transform(df[column])
+      label_mapping[column] = dict(zip(range(len(encoders[column].classes_)), encoders[column].classes_))
+
+  return df, label_mapping
+#endregion ****
+
+def process_dataset(df, municipi_dict, provincies_dict):
+    clean_df = (
+        df.pipe(generateMundissec)
+          .pipe(renameColumns, RENAMINGS)
+          .pipe(reduceColumns, COLUMNS_IN_USE)
+          .pipe(castColumns)
+          .pipe(groupSameMeaningValues, SAME_MEANING_VALUES)
+          .pipe(removeOutliers)
+          .pipe(regenerateCodes, municipi_dict, provincies_dict)
+    )
+
+    reduced_dataset, label_mapping = encode_categorical_columns(clean_df, CATEGORICAL_COLUMNS_TO_ENCODE)
+
+    data_dir = "data"
+    parquet_path = os.path.join(data_dir, "certificats.parquet")
+    json_path = os.path.join(data_dir, "labels.json")
+    
+    os.makedirs(data_dir, exist_ok=True)
+
+    reduced_dataset.to_parquet(parquet_path, engine="fastparquet", compression="GZIP")
+    
+    with open(json_path, "w") as f:
+        json.dump(label_mapping, f)
+
+    print("✅ Data cleaned and saved to", parquet_path)
+    print("✅ Label mapping saved to", json_path)
 
 df = pd.read_csv("data/raw_data.csv")
-df.to_parquet("data/processed_data.parquet", engine="fastparquet", compression="GZIP")
-print("✅ Data cleaned and saved to data/processed_data.parquet")
+process_dataset(df, municipi_dict, provincies_dict)
