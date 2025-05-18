9551e4abaa99f20e0571a32302b8cf9ca7af7557
Pol Darder <165882504+poldarder@users.noreply.github.com>
2025-05-18 18:49:03 +0200

    Dashboard data loading (#2)
    * Add timeline plot
    
    * Created Duck DB client class
    
    * Improve data filtering speed
    
    * Wip: mosaic dashboard
    
    * Cross filtering dashboard test

--- observablehq.config.js
+++ observablehq.config.js
@@ -7,7 +7,8 @@ export default {
   // all pages will be listed in alphabetical order. Listing pages explicitly
   // lets you organize them into sections and have unlisted pages.
   pages: [
-    {name: "Observatori", path: "observatori.html"}
+    {name: "Observatori", path: "observatori.html"},
+    {name: "Mosaic test", path: "mosaic.html"}
   ],
 
   // Content to add to the head of the page, e.g. for a favicon:
