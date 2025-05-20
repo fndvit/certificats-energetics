925a8385b49068112472cf3d25b175ca8c40acc5
Pol <poldama99@gmail.com>
2025-05-20 23:16:16 +0200

    Add cards, zona climatica barplot base

--- src/components/colors.js
+++ src/components/colors.js
@@ -8,12 +8,4 @@ export const qualifColorLookup = ({
   G: "#ea2038"
 });
 
-export const qualifColorScheme = [
-  "#3b7634",
-  "#5ea336",
-  "#a2cf2a",
-  "#f7df1b",
-  "#f18f20",
-  "#eb422bff",
-  "#ea2038"
-];
+export const categoricalScheme5 = ["#f6de19", "#3f6f94", "#daa4f9", "#bf3e05", "#35c8ef"];
